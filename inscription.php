5589a00fbc36befecc8dd588477b95a3771e81c9
CPacBellec <cbellec@normandiewebschool.fr>
2023-12-05 14:41:05 +0000

    major update + server activation

--- inscription.php
+++ inscription.php
@@ -1,41 +1,42 @@
-<?php
-include 'header.php';
-include 'config.php';
+<?php include 'header.php'; ?>
+<?php include 'config.php'; ?>
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nom = $_POST['nom'];
-    $email = $_POST['email'];
-    $mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT);
+<div class="container mx-auto p-4">
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nom = $_POST['nom'];
+        $email = $_POST['email'];
+        $mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT);
 
-    $result = $conn->query("SELECT COUNT(*) AS total FROM utilisateurs");
-    if ($result) {
-        $row = $result->fetch_assoc();
-        $total_utilisateurs = $row['total'];
+        $result = $conn->query("SELECT COUNT(*) AS total FROM utilisateurs");
+        if ($result) {
+            $row = $result->fetch_assoc();
+            $total_utilisateurs = $row['total'];
+        }
+        $role = ($total_utilisateurs == 0) ? 'admin' : 'utilisateur';
+
+        $stmt = $conn->prepare("INSERT INTO utilisateurs (nom, email, mot_de_passe, role) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $nom, $email, $mot_de_passe, $role);
+
+        if ($stmt->execute()) {
+            echo "<p class='text-green-500'>Inscription réussie.</p>";
+        } else {
+            echo "<p class='text-red-500'>Erreur lors de l'inscription: " . $stmt->error . "</p>";
+        }
+
+        $stmt->close();
     }
-    $role = ($total_utilisateurs == 0) ? 'admin' : 'utilisateur';
 
+    $conn->close();
+    ?>
 
-    $stmt = $conn->prepare("INSERT INTO utilisateurs (nom, email, mot_de_passe, role) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssss", $nom, $email, $mot_de_passe, $role);
-
-    if ($stmt->execute()) {
-        echo "Inscription réussie.";
-    } else {
-        echo "Erreur lors de l'inscription: " . $stmt->error;
-    }
-
-    $stmt->close();
-}
-
-$conn->close();
-?>
-
-<!-- Formulaire d'inscription -->
-<form method="post" action="inscription.php">
-    <label>Nom: <input type="text" name="nom" required></label><br>
-    <label>Email: <input type="email" name="email" required></label><br>
-    <label>Mot de passe: <input type="password" name="mot_de_passe" required></label><br>
-    <input type="submit" value="S'inscrire">
-</form>
+    <!-- Formulaire d'inscription -->
+    <form class="max-w-md mx-auto bg-white p-6 rounded-md shadow-md mt-4" method="post" action="inscription.php">
+        <label class="block mb-2">Nom: <input class="border p-2 w-full" type="text" name="nom" required></label>
+        <label class="block mb-2">Email: <input class="border p-2 w-full" type="email" name="email" required></label>
+        <label class="block mb-2">Mot de passe: <input class="border p-2 w-full" type="password" name="mot_de_passe" required></label>
+        <button class="bg-blue-500 text-white p-2 rounded-md cursor-pointer" type="submit">S'inscrire</button>
+    </form>
+</div>
 
 <?php include 'footer.php'; ?>
